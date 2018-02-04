ab7ae0b3369de078b9fdbb01a93a3b0a01ba674a
CasablancaC4Q <32021306+CasablancaC4Q@users.noreply.github.com>
2018-02-04 12:44:40 -0500

    Intial commit for in class

---
+++ app/src/main/java/comli/example/c4q/mapsjose/MainActivity.java
@@ -0,0 +1,21 @@
+package comli.example.c4q.mapsjose;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToMapsActivity(View view){
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
+
+    }
+}
